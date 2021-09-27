f3afe101b07bd45f7cc9c93b2c740cb71a18e312
AlekHesa <91420580+AlekHesa@users.noreply.github.com>
2021-09-27 23:40:09 +0700

    Poster + Intent

--- app/src/main/java/com/example/mobileappdev_project/Home_Menu.java
+++ app/src/main/java/com/example/mobileappdev_project/Home_Menu.java
@@ -5,19 +5,33 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 public class Home_Menu extends Fragment {
+    private ImageButton BtnHindia;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_home_menu,container,false);
+        View v = inflater.inflate(R.layout.activity_home_menu,container,false);
+
+        BtnHindia = v.findViewById(R.id.BtnHindia);
+        BtnHindia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity(),PosterScreen.class);
+                startActivity(i);
+            }
+        });
 
 
+
+    return v;
     }
 
 }
--- app/src/main/java/com/example/mobileappdev_project/PosterScreen.java
+++ app/src/main/java/com/example/mobileappdev_project/PosterScreen.java
@@ -4,11 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class BayarTiket extends AppCompatActivity {
+public class PosterScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bayar_tiket);
+        setContentView(R.layout.activity_screen_poster);
+        getSupportActionBar().hide();
     }
 }
