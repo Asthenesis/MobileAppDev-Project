5d9ec4b5b7fdcd6c05e4f345ad65fdc336155d5a
AlekHesa <91420580+AlekHesa@users.noreply.github.com>
2021-09-27 20:52:27 +0700

    Menu + Fragment Utama

---
+++ app/src/main/java/com/example/mobileappdev_project/Home_Menu.java
@@ -0,0 +1,23 @@
+package com.example.mobileappdev_project;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Home_Menu extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.activity_home_menu,container,false);
+
+        return v;
+    }
+
+}
