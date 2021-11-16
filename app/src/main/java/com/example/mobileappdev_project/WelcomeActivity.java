02efc859be522aa413447bb15982e99fa17cad37
Irfan Fari <78146723+Asthenesis@users.noreply.github.com>
2021-11-16 11:02:07 +0700

    landing + welcome screen

--- app/src/main/java/com/example/mobileappdev_project/WelcomeActivity.java
+++ app/src/main/java/com/example/mobileappdev_project/WelcomeActivity.java
@@ -2,7 +2,11 @@ package com.example.mobileappdev_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        getSupportActionBar().hide();
+
+        ImageButton btn = (ImageButton)findViewById(R.id.skip_later_button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
+            }
+        });
     }
 }
