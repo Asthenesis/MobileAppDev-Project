1bcb56c4aa11e80fa521266011527c8f7ccc6eed
Irfan Fari <78146723+Asthenesis@users.noreply.github.com>
2021-11-16 10:49:54 +0700

    update

---
+++ app/src/main/java/com/example/mobileappdev_project/LandingPage.java
@@ -0,0 +1,29 @@
+package com.example.mobileappdev_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class LandingPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing_page);
+
+        getSupportActionBar().hide();
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), WelcomeActivity.class));
+                finish();
+            }
+        }, 3000L);
+
+
+
+    }
+}
