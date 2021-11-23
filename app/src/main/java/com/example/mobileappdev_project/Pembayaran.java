a9bbbcebb842ae50605d01612b9994561c745fde
Irfan Fari <78146723+Asthenesis@users.noreply.github.com>
2021-11-23 16:03:09 +0700

    update

--- app/src/main/java/com/example/mobileappdev_project/Pembayaran.java
+++ app/src/main/java/com/example/mobileappdev_project/Pembayaran.java
@@ -10,5 +10,6 @@ public class Pembayaran extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pembayaran);
+        getSupportActionBar().hide();
     }
 }
--- app/src/main/java/com/example/mobileappdev_project/Profile.java
+++ app/src/main/java/com/example/mobileappdev_project/Profile.java
@@ -2,7 +2,10 @@ package com.example.mobileappdev_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Profile extends AppCompatActivity {
 
@@ -11,5 +14,14 @@ public class Profile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         getSupportActionBar().hide();
+
+        ImageButton btn = (ImageButton)findViewById(R.id.btnKembali);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Profile.this, MainActivity.class));
+            }
+        });
+
     }
 }
