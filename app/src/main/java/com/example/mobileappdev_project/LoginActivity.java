5dc5e2bcf114b3b70ccac2214dd89c43cfb6ceb6
Irfan Fari <78146723+Asthenesis@users.noreply.github.com>
2021-11-29 20:03:17 +0700

    update

--- app/src/main/java/com/example/mobileappdev_project/LoginActivity.java
+++ app/src/main/java/com/example/mobileappdev_project/LoginActivity.java
@@ -1,13 +1,36 @@
 package com.example.mobileappdev_project;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText edtEmail, edtPassword;
+    private Button nexthome;
+    final String URL_Login = "http://lockyourticket.000webhostapp.com/SignIn.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,14 +38,68 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
 
-        ImageButton btn = (ImageButton)findViewById(R.id.nexthome_btn);
+        edtEmail = findViewById(R.id.edtEmail);
+        edtPassword = findViewById(R.id.edtPassword);
+
+        ImageButton btn = (ImageButton) findViewById(R.id.nexthome_btn);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                String u_email = edtEmail.getText().toString().trim();
+                String u_pass = edtPassword.getText().toString().trim();
+
+                if (!u_email.isEmpty() || !u_pass.isEmpty()) {
+                    SignIn(u_email, u_pass);
+                } else {
+                    edtEmail.setError("Please insert your Email correctly!");
+                    edtPassword.setError("Please Insert your Password correctly!");
+                }
             }
         });
+    }
+
+    private void SignIn(String email, String password) {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_Login, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    String success = jsonObject.getString("success");
+                    JSONArray jsonArray = jsonObject.getJSONArray("SignIn");
+
+                    if (success.equals("1")) {
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                            Toast.makeText(LoginActivity.this, "Login Success!", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(intent);
+                        }
+                    } else if (success.equals("0")) {
+                        Toast.makeText(LoginActivity.this, "Wrong Email/Password", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(LoginActivity.this, "error", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(LoginActivity.this, "Error "+error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("email", email);
+                params.put("password", password);
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(LoginActivity.this);
+        requestQueue.getCache().clear();
+        requestQueue.add(stringRequest);
 
     }
+
 }
