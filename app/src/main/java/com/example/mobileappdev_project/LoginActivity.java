ce022de351d829791eb8c523e9a19431229a1121
Irfan Fari <78146723+Asthenesis@users.noreply.github.com>
2021-12-06 23:56:11 +0700

    update (test)

--- app/src/main/java/com/example/mobileappdev_project/LoginActivity.java
+++ app/src/main/java/com/example/mobileappdev_project/LoginActivity.java
@@ -28,78 +28,150 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
-    EditText edtEmail;
-    EditText edtPassword;
-    String email,password;
-    String url_kirim_data = "http://lockyourticket.000webhostapp.com/Login.php";
+    EditText tombolEmail;
+    EditText tombolPassword;
+    String uEmail, uPassword;
+    private static final String TAG_MAHASISWA = "data";
+    String url_kirim_data = "http://192.168.1.5/Login.php";
+
+//    void kirim_data(){
+//        txtEmail = edtEmail.getText().toString();
+//        txtPassword = edtPassword.getText().toString();
+//        RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+//        StringRequest stringRequest = new StringRequest(Request.Method.POST, url_kirim_data, new Response.Listener<String>() {
+//            @Override
+//            public void onResponse(String response) {
+//                try {
+//                    JSONObject jObj = new JSONObject(response);
+//                    int sukses = jObj.getInt("success");
+//                    if (sukses == 1) {
+//                        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+//                        startActivity(i);
+//                        Toast.makeText(LoginActivity.this, "Data pengguna berhasil login", Toast.LENGTH_SHORT).show();
+//                        finish();
+//                    } else {
+//                        Toast.makeText(LoginActivity.this, "Data pengguna gagal disimpan", Toast.LENGTH_SHORT).show();
+//                    }
+//                } catch (Exception ex) {
+//                    Log.e("Error", ex.toString());
+//                }
+//            }
+//        },
+//                new Response.ErrorListener(){
+//                    @Override
+//                    public void onErrorResponse(VolleyError error) {
+//                        Log.e("Error", error.getMessage());
+//                        Toast.makeText(LoginActivity.this, "silahkan cek koneksi internet anda", Toast.LENGTH_SHORT).show();
+//                    }
+//                }) {
+//            protected Map<String, String> getParams() {
+//                Map<String, String> params = new HashMap<>();
+//                params.put("email", txtEmail);
+//                params.put("password", txtPassword);
+//                return params;
+//            }
+//            @Override
+//            public Map<String, String> getHeaders() throws AuthFailureError {
+//                Map<String, String> params = new HashMap<>();
+//                params.put("Content-Type", "application/x-www-form-urlencoded");
+//                return params;
+//            }
+//        };
+//
+//        queue.getCache().clear();
+//        queue.add(stringRequest);
+//
+//    }
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_login);
+//        getSupportActionBar().hide();
+//        edtEmail = (EditText) findViewById(R.id.edtEmail);
+//        edtPassword = (EditText) findViewById(R.id.edtPassword);
+//
+//        Button btnLogin = (Button) findViewById(R.id.btnLogin);
+//        btnLogin.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                kirim_data();
+//            }
+//        });
+//
+//    }
+//
+//
+//}
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_login);
+    getSupportActionBar().hide();
+
+    Button btn = (Button)findViewById(R.id.btnLogin);
 
 
-    void kirim_data(){
-        email = edtEmail.getText().toString();
-        password = edtPassword.getText().toString();
-        RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, url_kirim_data, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                try {
-                    JSONObject jObj = new JSONObject(response);
-                    int sukses = jObj.getInt("success");
-                    if (sukses == 1) {
-                        Intent i = new Intent(getApplicationContext(), MainActivity.class);
-                        startActivity(i);
-                        Toast.makeText(LoginActivity.this, "Data pengguna berhasil login", Toast.LENGTH_SHORT).show();
-                        finish();
-                    } else {
-                        Toast.makeText(LoginActivity.this, "Data pengguna gagal disimpan", Toast.LENGTH_SHORT).show();
+    tombolEmail = (EditText) findViewById(R.id.edtEmail);
+    tombolPassword = (EditText) findViewById(R.id.edtPassword);
+
+    btn.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            uEmail = tombolEmail.getText().toString();
+
+            uPassword = tombolPassword.getText().toString();
+            RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, url_kirim_data, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    try {
+                        JSONObject jObj = new JSONObject(response);
+                        int sukses = jObj.getInt("success");
+                        if (sukses == 1) {
+//                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                            Toast.makeText(LoginActivity.this, "User Berhasil Registrasi", Toast.LENGTH_SHORT).show();
+                            finish();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Gagal Registrasi, silahkan coba lagi", Toast.LENGTH_SHORT).show();
+                        }
+                        // progressBar.setVisibility(View.GONE);
+                    } catch (Exception ex) {
+                        Log.e("Error", ex.toString());
+                        //progressBar.setVisibility(View.GONE);
                     }
-                } catch (Exception ex) {
-                    Log.e("Error", ex.toString());
                 }
-            }
-        },
-                new Response.ErrorListener(){
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.e("Error", error.getMessage());
-                        Toast.makeText(LoginActivity.this, "silahkan cek koneksi internet anda", Toast.LENGTH_SHORT).show();
-                    }
-                }) {
-            protected Map<String, String> getParams() {
-                Map<String, String> params = new HashMap<>();
-                params.put("email", email);
-                params.put("password", password);
-                return params;
-            }
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                Map<String, String> params = new HashMap<>();
-                params.put("Content-Type", "application/x-www-form-urlencoded");
-                return params;
-            }
-        };
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("Error", error.getMessage());
+                    Toast.makeText(LoginActivity.this, "silahkan cek koneksi internet anda", Toast.LENGTH_SHORT).show();
+                    //progressBar.setVisibility(View.GONE);
+                }
+            }) {
+                @Override
+                protected Map<String, String> getParams() {
+                    Map<String, String> params = new HashMap<>();
 
-        queue.getCache().clear();
-        queue.add(stringRequest);
+                    params.put("password",uPassword);
+                    params.put("email", uEmail);
+                    return params;
+                }
 
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        getSupportActionBar().hide();
-        edtEmail = (EditText) findViewById(R.id.edtEmail);
-        edtPassword = (EditText) findViewById(R.id.edtPassword);
-
-        ImageButton btnLogin =  findViewById(R.id.nexthome_btn);
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                kirim_data();
-            }
-        });
-
-    }
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    Map<String, String> params = new HashMap<>();
+                    params.put("Content-Type", "application/x-www-form-urlencoded");
+                    return params;
+                }
+            };
+            queue.getCache().clear();
+            queue.add(stringRequest);
+        }
+    });
+}
 }
 
 /*public class LoginActivity extends AppCompatActivity {
